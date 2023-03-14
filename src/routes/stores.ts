3dba880d7ed6c72092df7710b16ae316bb9b4282
Libertas <1adamsvoboda@gmail.com>
2023-03-14 14:31:36 +0100

    map change and enhancement

---
+++ src/routes/stores.ts
@@ -0,0 +1,11 @@
+import { writable } from 'svelte/store';
+
+export const m1Active = writable<boolean>(true);
+export const m2Active = writable<boolean>(true);
+export const m3Active = writable<boolean>(true);
+export const l1Active = writable<boolean>(true);
+export const l2Active = writable<boolean>(true);
+export const l3Active = writable<boolean>(true);
+export const l4Active = writable<boolean>(true);
+export const l5Active = writable<boolean>(true);
+export const allActive = writable<boolean>(true);
