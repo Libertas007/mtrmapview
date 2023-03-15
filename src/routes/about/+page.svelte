b1cf94c9b22b038d3b32db3756d4469f7ca8381a
Libertas <1adamsvoboda@gmail.com>
2023-03-15 14:49:29 +0100

    added Word'n'Seek link

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,4 +7,10 @@
 	<h1>About this project</h1>
 
 	<p>This project is a interactive map of my MTR world in Minecraft.</p>
+
+	<h2>Other projects</h2>
+	<p>
+		I'd be very glad if you download my word game Word'n'Seek. Available to download on Android and
+		iOS <a href="https://word-n-seek.com/">here</a>.
+	</p>
 </div>
